891dd8abbd057279446003dbc69959cb7b518d2a
Jereme Allen <jereme.allen@wheniwork.com>
2017-04-21 13:02:20 -0500

    Add in custom component and styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <tweet></tweet>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+import Tweet from './components/Tweet';
 
 export default {
   name: 'app',
+  data() {
+    return {};
+  },
+  computed: {
+    tweetIsEmpty() {
+      return this.tweet.length === 0;
+    },
+  },
   components: {
-    Hello,
+    Tweet,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Tweet.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="w-75 center ba b--black-10" id="twitterVue">
+    <div class="pv2 tc bb b--black-10">
+      <h1 class="ma0 f5 normal">Compose New Tweet</h1>
+    </div>
+    <div class="bg-near-white pa3">
+      <textarea placeholder="Write your tweet here" name="tweet" v-model="tweet" rows="3" class="w-100 br2 ba b--black-10 pa2"></textarea>
+      <div class="mt3 tr">
+        <button :disabled="tweetIsEmpty" class="button-reset bg-blue bn white f6 fw5 pv2 ph3 br2 dim">Tweet</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
